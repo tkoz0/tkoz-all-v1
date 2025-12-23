279414a04dbaaf8ec2bf04bed82777aae239baec
tkoz <tkoz@tkoz.me>
2025-12-22 23:40:02 -0500

    Added something for the possibility of using either GCC or Clang

--- cmake/run_cmake_generate.sh
+++ cmake/run_cmake_generate.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 source "$(dirname $0)/bash_common.sh"
 check_build_type $1
-cmake -S "$(dirname $0)/.." -B "$(dirname $0)/../builds/$1" -DCMAKE_BUILD_TYPE=$1
+cmake -S "$(dirname $0)/.." -B "$(dirname $0)/../builds/$1" \
+    -DCMAKE_BUILD_TYPE=$1 \
+    -DCMAKE_TOOLCHAIN_FILE="$(dirname $0)/toolchains/clang.cmake"
