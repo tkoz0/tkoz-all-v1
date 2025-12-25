e5940c698a7e2a81e3e9a311eae40ddd92fcdf2a
tkoz <tkoz@tkoz.me>
2025-12-25 18:12:50 -0500

    added coverage build type, name build areas with compiler choice

--- cmake/tkoz_options_common.cmake
+++ cmake/tkoz_options_common.cmake
@@ -2,17 +2,35 @@ add_library(tkoz_options_common INTERFACE)
 target_compile_features(tkoz_options_common INTERFACE cxx_std_23)
 
 # Always enable all warnings, if they were intended then they can be suppressed
-target_compile_options(tkoz_options_common INTERFACE -Wall -Wextra -Wpedantic)
+target_compile_options(tkoz_options_common INTERFACE
+    -Wall -Wextra -Wpedantic)
 
 # Note: address/undefined sanitizers may cause issues for intentional low level code
-target_compile_options(tkoz_options_common INTERFACE $<$<CONFIG:Debug>: -g -O0 -fsanitize=address,undefined -fno-omit-frame-pointer >)
-target_link_options(tkoz_options_common INTERFACE $<$<CONFIG:Debug>: -fsanitize=address,undefined >)
+target_compile_options(tkoz_options_common INTERFACE
+    $<$<CONFIG:Debug>: -g -O0 -fsanitize=address,undefined -fno-omit-frame-pointer >)
+target_link_options(tkoz_options_common INTERFACE
+    $<$<CONFIG:Debug>: -fsanitize=address,undefined >)
 
-target_compile_options(tkoz_options_common INTERFACE $<$<CONFIG:RelWithDebInfo>: -g -O2 -Werror >)
+target_compile_options(tkoz_options_common INTERFACE
+    $<$<CONFIG:RelWithDebInfo>: -g -O2 >)
 
-target_compile_options(tkoz_options_common INTERFACE $<$<CONFIG:MinSizeRel>: -Os -Werror >)
+target_compile_options(tkoz_options_common INTERFACE
+    $<$<CONFIG:MinSizeRel>: -Os -Werror >)
 
-target_compile_options(tkoz_options_common INTERFACE $<$<CONFIG:Release>: -O3 -Werror >)
+target_compile_options(tkoz_options_common INTERFACE
+    $<$<CONFIG:Release>: -O3 -Werror >)
 
-# Define TEST macro for Debug and RelWithDebInfo builds
-target_compile_definitions(tkoz_options_common INTERFACE $<$<OR:$<CONFIG:Debug>,$<CONFIG:RelWithDebInfo>>: TEST >)
+target_compile_options(tkoz_options_common INTERFACE
+    $<$<CONFIG:Coverage>: -g -O0 >)
+
+if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+    target_compile_options(tkoz_options_common INTERFACE $<$<CONFIG:Coverage>: --coverage >)
+    target_link_options(tkoz_options_common INTERFACE $<$<CONFIG:Coverage>: --coverage >)
+elseif (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+    target_compile_options(tkoz_options_common INTERFACE $<$<CONFIG:Coverage>: -fprofile-instr-generate -fcoverage-mapping >)
+    target_link_options(tkoz_options_common INTERFACE $<$<CONFIG:Coverage>: -fprofile-instr-generate >)
+endif()
+
+# Define TEST macro for Debug, RelWithDebInfo, and Coverage
+target_compile_definitions(tkoz_options_common INTERFACE
+    $<$<OR:$<CONFIG:Debug>,$<CONFIG:RelWithDebInfo>,$<CONFIG:Coverage>>: TEST >)
---
+++ scripts/bash_common.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# usage: check_build_type $1
+# exit status is 0 when valid
+# when build type is invalid, prints message and exits
+check_build_type() {
+    local build_types=(Debug Release RelWithDebInfo MinSizeRel Coverage)
+    local is_valid=1
+    for build_type in "${build_types[@]}"; do
+        if [[ "$1" == "$build_type" ]]; then
+            is_valid=0
+            break
+        fi
+    done
+    if [[ $is_valid -ne 0 ]]; then
+        echo not a valid build type
+        echo valid build types are: "${build_types[@]}"
+        exit 1
+    fi
+    return $is_valid
+}
+
+# usage: check_compiler_type $1
+# exit status is 0 when valid
+# when compiler type is invalid, prints message and exits
+check_compiler_type() {
+    local compiler_types=(GCC Clang)
+    local is_valid=1
+    for compiler_type in "${compiler_types[@]}"; do
+        if [[ "$1" == "$compiler_type" ]]; then
+            is_valid=0
+            break
+        fi
+    done
+    if [[ $is_valid -ne 0 ]]; then
+        echo not a valid compiler type
+        echo valid compiler types are: "${compiler_types[@]}"
+        exit 1
+    fi
+    return $is_valid
+}
+
+# usage: toolchain_file $1
+# prints the file name to stdout, exits if invalid
+toolchain_file() {
+    if [[ "$1" == "GCC" ]]; then
+        echo 'gcc.cmake'
+    elif [[ "$1" == "Clang" ]]; then
+        echo 'clang.cmake'
+    else
+        echo not a valid compiler type
+        exit 1
+    fi
+}
